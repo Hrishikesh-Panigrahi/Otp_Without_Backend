27e1a483b2c814130812e3a756d639a771ec6051
Hrishikesh-Panigrahi <hrishikesh.panigrahi38@gmail.com>
2024-10-11 16:26:46 +0530

    refactored code and added post method

--- controller/controller.go
+++ controller/controller.go
@@ -8,19 +8,26 @@ import (
 	"github.com/Hrishikesh-Panigrahi/Otp_Without_Backend/utils"
 )
 
+// UserInput is a controller function to handle the user input
 func UserInput(w http.ResponseWriter, r *http.Request) {
-	otp := utils.GenerateOTP(6)
-	ttl := 5 * 60
-	expires := time.Now().Add(time.Duration(ttl) * time.Second).Format("2006-01-02 15:04:05")
-	data := fmt.Sprintf(otp + "." + expires + "." + "your-secure-key")
 
-	hash := utils.CreateHash(data)
+	if r.Method == "POST" {
+		// Generate a 6 digit OTP
+		otp := utils.GenerateOTP(6)
 
-	fmt.Printf("\n" + hash)
+		// Set the expiry time of the OTP
+		ttl := 5 * 60
+		expires := time.Now().Add(time.Duration(ttl) * time.Second).Format("2006-01-02 15:04:05")
+		
+		// Create a hash of the OTP, expiry time and a secure key
+		data := fmt.Sprintf(otp + "." + expires + "." + "your-secure-key")
+		hash := utils.CreateHash(data)
+		
+		// Create a full hash by concatenating the hash and the expiry time
+		fullHash := hash + "." + expires
 
-	fullHash := hash + "." + expires
+		fmt.Printf("\n" + hash)
+		fmt.Printf("\n" + fullHash)
+	}
 
-	fmt.Printf("\n" + fullHash)
-
-	w.Write([]byte(fullHash))
 }
--- utils/generateOTP.go
+++ utils/generateOTP.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"math"
 	"math/big"
-
 )
 
 func GenerateOTP(maxDigits uint32) string {
